3df1262125bb5e9e0ad9bddcc0259d19c3c1cd45
selin.vik <selinvik.dev@gmail.com>
2023-11-15 14:54:38 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Specify the output directory for the production build
+    minify: true, // Enable minification for production build
+    sourcemap: false, // Disable sourcemaps for production build (optional)
+    terserOptions: {
+      compress: {
+        drop_console: true, // Drop console.* statements in production
+        drop_debugger: true, // Drop debugger statements in production
+      },
+    },
+  },
 });
